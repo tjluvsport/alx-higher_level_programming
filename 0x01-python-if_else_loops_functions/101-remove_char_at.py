8b92e939d6fadf2c6fb924695a40cd0a13f723ca
tjluvsport <tjluvsport@gmail.com>
2023-01-03 12:39:38 -0800

    Task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for char in range(122, 96, -1):
-    print("{:c}".format(char if (char % 2 == 0) else (char - 32)), end='')
+def remove_char_at(str, n):
+    if n >= 0:
+        new_str = str[:n] + str[n + 1:]
+        return new_str
+    else:
+        return str
