dabf29c0619bb3f2371f520a9ae3f5ce55afabee
tjluvsport <tjluvsport@gmail.com>
2023-01-30 09:41:59 -0800

    Classes and objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except IndexError:
-            pass
-    print()
-    return count
+"""Define a class Square"""
+
+
+class Square:
+    """Define an empty class named Square"""
+    pass
